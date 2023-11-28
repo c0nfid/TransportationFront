33fe7616808fb57e87e2c1618823bd177fc8e2b4
c0nfid <beiov2004.mail@gmail.com>
2023-11-29 01:08:20 +0300

    Just try to fix modal input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {createContext, useEffect, useState} from "react";
 import './styles/App.css';
 import Menu from "./ui/Menu";
 import Header from "./ui/Header";
@@ -6,14 +6,20 @@ import TableComponent from "./ui/TableComponent";
 import $api from "./http/api";
 import axios from "axios";
 import Table from "./components/Table/Table";
-
+import ModalWindow from "./ui/ModalWindow/ModalWindow";
+import EntityModifier from "./components/Table/EntityModifier/EntityModifier";
+export const ApplicationContext = createContext(null)
 
 function App() {
     const [data, setData] = useState([]);
 
+    const addRow = () => {
+
+    }
     async function fetchAutos(){
         const response = await axios.get('http://127.0.0.1:8000/models/')
         setData(response.data)
+
     }
 
     useEffect(() => {
@@ -22,14 +28,28 @@ function App() {
     }, []);
 
 
+    const [isModalVisible, setIsModalVisible] = useState(false)
+    const [entity, setEntity] = useState(11)
+    const [entityData, setEntityData] = useState({})
+
+    // useEffect(() => {
+    //     const temp = data
+    //     temp[entity] = entityData
+    //     console.log(isModalVisible)
+    //
+    //
+    // }, [isModalVisible]);
   return (
     <div className="App">
+        <ApplicationContext.Provider value={{entity, setEntity}}>
+        <ModalWindow isVisible={isModalVisible} setIsVisible={setIsModalVisible} ><EntityModifier {...{entityData, setEntityData}}/></ModalWindow>
         <Menu />
         <div className="Body">
             <Header nameTable='Автопарк' date='17.11.2023'/>
-            <Table data={data} />
+            <Table data={data} {...{isModalVisible, setIsModalVisible, setEntityData}} /> // setEntitydata
             <button onClick={fetchAutos}>update</button>
         </div>
+        </ApplicationContext.Provider>
     </div>
   );
 }
